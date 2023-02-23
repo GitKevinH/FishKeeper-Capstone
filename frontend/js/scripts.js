0910d4749bafc63ad7c3b4db3e58abeee9025bbf
Kevin Hoang <118770135+GitKevinH@users.noreply.github.com>
2023-02-23 09:47:51 -0500

    Added some front-end work along with ER diagram.

---
+++ frontend/js/scripts.js
@@ -0,0 +1,33 @@
+/*!
+* Start Bootstrap - Business Frontpage v5.0.8 (https://startbootstrap.com/template/business-frontpage)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+
+let feature = ["Record your catch","Check your stats","Map your spots (coming soon)"]
+  document.getElementById("feature1").innerHTML = feature[0];
+  document.getElementById("feature2").innerHTML = feature[1];
+  document.getElementById("feature3").innerHTML = feature[2];
